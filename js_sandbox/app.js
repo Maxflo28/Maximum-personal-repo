110d9b3e3b7751828f0f1a68f50aeed1c1e3392d
Max Flournoy <Maxflo13@gmail.com>
2022-02-01 13:39:44 -0800

    updated data types

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -15,10 +15,40 @@
 
 // var, let, const
 
-var name = 'John Doe';
-console.log(name);
-name = 'Steve Smith';
-console.log(name);
+// var name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
 
 // Init var
 
+// PRIMITIVE
+
+// string
+// const name = 'john doe';
+
+// console.log(typeof name);
+
+// number
+const age = 45;
+// boolean
+const hasKids = true;
+// null
+const car = null;
+// undefined
+let test;
+// symbol
+const sym = Symbol();
+
+// Reference types - objects
+// array
+const hobbies = ['movies', 'music'];
+// Object literal
+const address = {
+  city: 'Boston',
+  state: 'MA'
+}
+const today = new Date();
+
+console.log(today);
+console.log(typeof today);
