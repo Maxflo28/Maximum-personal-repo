d472d28b40298fd4c48af12da8e5c787ad3b2027
Max Flournoy <Maxflo13@gmail.com>
2022-02-02 19:43:01 -0800

    javascript practice

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -55,43 +55,147 @@
 
 
 
-let val; 
+// let val; 
 
-// Number to string
-val = 5;
-val = String(4+4);
-// Bool to string
-val = String(true);
-// Date to string
-val = String(new Date());
-// Array to string 
-val = String([1,2,3,4]);
+// // Number to string
+// val = 5;
+// val = String(4+4);
+// // Bool to string
+// val = String(true);
+// // Date to string
+// val = String(new Date());
+// // Array to string 
+// val = String([1,2,3,4]);
 
-// toString()
-val = (5).toString();
-val = (true).toString();
+// // toString()
+// val = (5).toString();
+// val = (true).toString();
 
-// String to number
-val = '5';
-
-
+// // String to number
+// val = '5';
 
 
-// Output
-console.log(val);
-console.log(typeof val);
-// console.log(val.length);
-// console.log(val.toFixed());
 
-// Math Object
-val = Math.PI;
-val = Math.E;
-val = Math.round(2.4);
-val = Math.ceil(2.4);
-val = Math.sqrt(64);
-val = Math.abs(-3);
-val = Math.pow(8, 2);
 
-val = Math.floor(Math.random() * 20 + 1);
+// // Output
+// console.log(val);
+// console.log(typeof val);
+// // console.log(val.length);
+// // console.log(val.toFixed());
 
-console.log(val);
+// // Math Object
+// val = Math.PI;
+// val = Math.E;
+// val = Math.round(2.4);
+// val = Math.ceil(2.4);
+// val = Math.sqrt(64);
+// val = Math.abs(-3);
+// val = Math.pow(8, 2);
+
+// val = Math.floor(Math.random() * 20 + 1);
+
+// console.log(val);
+
+
+// const firstName = 'William';
+// const lastName = 'Johnson';
+
+// let val;
+
+// val = firstName + lastName;
+
+// // Concatenation
+
+// val = firstName + ' Supreme ' + lastName;
+
+// console.log(val);
+
+
+// // Append
+// val = 'Brad ';
+// val += 'Traversy';
+// age = 29;
+// val = 'Hello, my name is ' + firstName + ' and I am ' + age;
+
+// // Escaping
+// val = 'That\'s awesome, I can\'t wait';
+
+// // concat
+// val = firstName.concat(' ', lastName);
+
+// console.log(val);
+
+// // Change case
+// val = firstName.toUpperCase();
+
+
+// const name = 'John';
+// const age = 29;
+// const job = 'Web Developer';
+// const city = 'Miami';
+
+// // Without template strings (es5)
+
+// html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + '</li><li>City: ' + city + '</li><ul>'; 
+
+// html = '<ul>' +
+//         '<li>Name: ' + name + '</li>' +
+//         '<li>Age: ' + age + '</li>' +
+//         '<li>Job: ' + job + '</li>' +
+//         '<li>City=: ' + city + '</li>' +
+//         '</ul>';
+
+// function hello(){
+//   return 'hello';
+// }
+
+// // With template strings (es6)
+// html = `
+//   <ul>
+//     <li>Name: ${name}</li>
+//     <li>Age: ${age}</li>
+//     <li>Job: ${job}</li>
+//     <li${2 + 2}</li>
+//     <li${hello()}</li>
+
+//   </ul>
+// `; 
+
+
+
+// document.body.innerHTML = html;
+
+
+// Create some arrays
+// const numbers = [4, 2, 6, 9];
+// const numbers2 = new Array(11, 22, 343);
+// const fruit = [apple. orange. grapes];
+// const mixed = [22, 'hello', true, undefined, null, new Date()];
+
+// let val;
+
+// // get array length
+// val = numbers.length;
+// // check if is array
+// val = Array.isArray(numbers);
+
+// console.log(numbers);
+// console.log(val);
+
+
+// // Mutating Arrays
+// // Add on to end
+// numbers.push(numbers);
+// // Add to front
+// numbers.unshift(2);
+// // Take off from front
+// numbers.shift();
+// // Splice values 
+// numbers.splice(1,1) //(START, END)
+
+// // Reverse sort
+// val = numbers.sort(function(x, y){
+  
+// })
+
+
